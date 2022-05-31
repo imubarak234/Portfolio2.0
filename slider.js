97fe44b350c68af1ff5b53142da9a107a3bc8572
Mubarak <imubarak2424@gmail.com>
2022-05-31 19:54:41 +0100

    screen changes sliders

--- slider.js
+++ slider.js
@@ -1,4 +1,4 @@
-const sliderDiv = document.querySelector();
+const sliderDiv = document.querySelector('#carouselExampleControls101');
 
 const mainSlider = `<div class="carousel-inner">
 <div class="carousel-item active">
@@ -130,3 +130,101 @@ const mainSlider = `<div class="carousel-inner">
 <span class="carousel-control-next-icon" aria-hidden="true"></span>
 <span class="visually-hidden">Next</span>
 </button>`;
+
+const mobileSlider = `<div class="carousel-inner">
+<div class="carousel-item active">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">Calculator</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">Awesome Books</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">MazeFlix</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">Wordpress Projects</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">Awesome Books</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">Wordpress Projects</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="card shadow bg-body rounded">
+    <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">Awesome Books</h4>
+      <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi molestias incidunt dolore? Facilis, iste delectus!
+      </p>
+      <button type="button" class="btn btn-lg project-button">See Project</button>
+    </div>
+  </div>
+</div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls101" data-bs-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls101" data-bs-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="visually-hidden">Next</span>
+</button>`;
+
+
+console.log(window.screen.availWidth);
