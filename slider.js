701ff45bd983f81385e51ecd5b01c82a83fe6103
Mubarak <imubarak2424@gmail.com>
2022-05-31 18:11:37 +0100

    main slider

---
+++ slider.js
@@ -0,0 +1,132 @@
+const sliderDiv = document.querySelector();
+
+const mainSlider = `<div class="carousel-inner">
+<div class="carousel-item active">
+  <div class="row g-4">
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">Calculator</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">To-Do-List</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">Leaderboard</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="row g-4">
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">Wordpress Projects</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">Awesome Books</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">MazeFlix</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="carousel-item">
+  <div class="row g-4">
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">Wordpress Projects</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">Awesome Books</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card shadow bg-body rounded">
+        <img src="./images/pexels-pixabay-373076.jpg" alt="" class="img-fluid card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">MazeFlix</h4>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+             Modi molestias incidunt dolore? Facilis, iste delectus!
+          </p>
+          <button type="button" class="btn btn-lg project-button">See Project</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls101" data-bs-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls101" data-bs-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="visually-hidden">Next</span>
+</button>`
